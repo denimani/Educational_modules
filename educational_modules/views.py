e6a69dcce26b0a5aca1fbd65cfcdc21da33966cc
Denis <isxakov.den@bk.ru>
2023-10-11 21:35:18 +0500

    Added views

--- educational_modules/views.py
+++ educational_modules/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from models import EducationalModule
+from serializers import EducationalModuleSerializer
+
+
+class EducationalModuleListCreateView(CreateAPIView):
+    serializer_class = EducationalModuleSerializer
+
+
+class EducationalModuleRetrieveView(RetrieveAPIView):
+    queryset = EducationalModule.objects.all()
+    serializer_class = EducationalModuleSerializer
+
+
+class EducationalModuleUpdateView(UpdateAPIView):
+    queryset = EducationalModule.objects.all()
+    serializer_class = EducationalModuleSerializer
+
+
+class EducationalModuleDestroyView(DestroyAPIView):
+    queryset = EducationalModule.objects.all()
