c77e0a5eaf87400dad07cba1e541d6e023953d3a
Denis <isxakov.den@bk.ru>
2023-10-11 21:21:37 +0500

    Created model "EducationsModule"

--- educational_modules/models.py
+++ educational_modules/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 
-# NULLABLE = {
-#     'null': True,
-#     'blank': True,
-# }
-#
-#
-# class EducationalModule(models.Model):
-#     name = models.CharField(max_length=255, verbose_name='Название')
-#     order_number = models.IntegerField(verbose_name='Порядковый номер')
-#     description = models.TextField(verbose_name='Описание')
-#
-#     def __str__(self):
-#         return self.name
-#
-#     class Meta:
-#         verbose_name = 'Образовательный модуль'
-#         verbose_name_plural = 'Образовательные модули'
-#
+NULLABLE = {
+    'null': True,
+    'blank': True,
+}
+
+
+class EducationalModule(models.Model):
+    """
+    Модель образовательного модуля
+    """
+    name = models.CharField(max_length=255, verbose_name='Название')
+    order_number = models.IntegerField(unique=True, verbose_name='Порядковый номер')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Образовательный модуль'
+        verbose_name_plural = 'Образовательные модули'
