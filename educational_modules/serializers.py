ff58517e381b0325cc70e8c941996ae1b59485f3
Denis <isxakov.den@bk.ru>
2023-10-11 21:23:59 +0500

    Added serializer

---
+++ educational_modules/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from models import EducationalModule
+
+
+class EducationalModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EducationalModule
+        fields = '__all__'
